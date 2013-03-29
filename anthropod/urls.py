6c1050c090e710f380dae90542cc36d650e06448
Thom Neale <twneale@gmail.com>
2013-03-29 13:06:54 -0400

    set admin as url prefix

--- anthropod/urls.py
+++ anthropod/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('',
-    url(r'^/', include('anthropod.admin.urls')),
+    url(r'^admin/', include('anthropod.admin.urls')),
 )
