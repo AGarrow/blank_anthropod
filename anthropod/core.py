a1690c57caa9fcc63fce1280081b61eddd69e548
Thom Neale <twneale@gmail.com>
2013-04-01 16:53:16 -0400

    added son_manipulator

--- anthropod/core.py
+++ anthropod/core.py
@@ -1,4 +1,5 @@
 import pymongo
+from pymongo.son_manipulator import SONManipulator
 
 from django.conf import settings
 
@@ -12,13 +13,29 @@ class ErrorProxy(object):
 
 
 db = None
+_model_registry = {}
+_model_registry_by_collection = {}
 
 
 def _configure_db(host, port, db_name):
+
     global db
+
+    class Transformer(SONManipulator):
+
+        def transform_outgoing(
+                self, son, collection,
+                mapping=_model_registry_by_collection):
+
+            try:
+                return mapping[collection.name](son)
+            except KeyError:
+                return son
+
     try:
         conn = pymongo.MongoClient(host, port)
         db = conn[db_name]
+        db.add_son_manipulator(Transformer())
     # return a dummy NoDB object if we couldn't connect
     except (pymongo.errors.AutoReconnect,
             pymongo.errors.ConnectionFailure) as e:
@@ -29,3 +46,6 @@ def _configure_db(host, port, db_name):
 _configure_db(settings.MONGO_HOST,
               settings.MONGO_PORT,
               settings.MONGO_DATABASE)
+
+
+from .models import *
