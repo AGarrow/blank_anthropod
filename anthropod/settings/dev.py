812baf66fc7ac7587cada6176892c2308971446c
Thom Neale <twneale@gmail.com>
2013-04-01 11:27:25 -0400

    Removed debug settings from dev settings

---
+++ anthropod/settings/dev.py
@@ -0,0 +1,10 @@
+from anthropod.settings.base import *
+
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+SECRET_KEY = '7d05%rr+^+12-v1q%%u$e7=-7-83rxu((n3j3zz9%rit-#xx5q'
+
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+MONGO_DATABASE = 'ocd'
