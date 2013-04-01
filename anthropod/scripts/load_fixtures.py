342947cd500e92dbaa54dbd090c7ea99a4d3ca5d
Thom Neale <twneale@gmail.com>
2013-04-01 17:37:46 -0400

    If no args given, load everything.

--- anthropod/scripts/load_fixtures.py
+++ anthropod/scripts/load_fixtures.py
@@ -2,11 +2,13 @@ import sys
 
 
 def main():
-    fixture_modules = sys.argv[1:]
+    fixture_modules = sys.argv[1:] or ['organization', 'person']
+
     usage = ('usage: python ./scripts/load_fixtures.py '
              '[fixture_module1[, fixture_modeul2]...]')
     if not fixture_modules:
         print usage
+
     for fixture_module in fixture_modules:
         mod_fqname = 'anthropod.admin.%s.fixtures' % fixture_module
         mod = __import__(mod_fqname, globals(), locals(), ['object'], -1)
