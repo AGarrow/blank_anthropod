785fbd00ddd87d3415a810aa3ef57599637378f8
Thom Neale <twneale@gmail.com>
2013-04-09 10:19:56 -0400

    give up on silly tab key binding

--- anthropod/collect/static/js/site/forms.js
+++ anthropod/collect/static/js/site/forms.js
@@ -10,35 +10,25 @@ $(document).ready(function() {
 
   function setup_row_events(){
 
-    console.log('binding...');
-
     /* When user clicks the button to add a new row,
     create a new tr node, append the row template
     content to it, and append it to the table. */
     var add_another = $('.add-another'),
-        last_input = $('.tab-add-row :last').children(),
+        last_input = $('.tab-add-row').children(),
         x_button = $('.remove-row');
 
     // Nuke old events.
     add_another.unbind('click');
-    last_input.unbind('keydown', 'tab');
     x_button.unbind('click');
 
-    // Add a new one.
+    // Add a new row.
     add_another.click(function(){
       var row = $('<tr></tr>');
       row.append(row_templates[$(this).data('fieldname')]);
       $(this).find('~table:first tbody').append(row);
       $(document).trigger('row:add');
-    });
-
-    /* Add a new row when user presses tab in last cell of last row. */
-    last_input.bind('keydown', 'tab', function(e){
-      e.preventDefault();
-      $(this).parents('div.row').find('a.btn').click();
-      $(this).parents('tr').next().find(":input :first").focus();
       return false;
-      });
+    });
 
     /* Remove the form row if user clicks the x button. */
     x_button.click(function(){
