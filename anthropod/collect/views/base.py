c03d5fa0b877b5a013ef5edb9ca2959620500db2
Thom Neale <twneale@gmail.com>
2013-05-31 14:11:28 -0400

    Make check_perms accessible on the RestrictedView class

--- anthropod/collect/views/base.py
+++ anthropod/collect/views/base.py
@@ -2,8 +2,12 @@ from django.views.generic.base import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
+from ..permissions import check_permissions
+
 
 class RestrictedView(View):
+
+    check_permissions = check_permissions
 
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
