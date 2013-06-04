93c9447d3026755efe461c40ce5c00c11d4d0451
Thom Neale <twneale@gmail.com>
2013-06-04 14:36:08 -0400

    Add test user_db

--- anthropod/tests/__init__.py
+++ anthropod/tests/__init__.py
@@ -3,8 +3,9 @@ from anthropod.core import settings, _configure_db
 
 
 settings.MONGO_DATABASE += '_test'
+settings.MONGO_USER_DATABASE += '_test'
 _configure_db(settings.MONGO_HOST, settings.MONGO_PORT,
-              settings.MONGO_DATABASE,)
+              settings.MONGO_DATABASE, settings.MONGO_USER_DATABASE)
 
 
 def teardown():
