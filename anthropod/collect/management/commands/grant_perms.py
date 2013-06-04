286c2748e5e2badc7b42dfa5884cd62aefbfcfa4
Thom Neale <twneale@gmail.com>
2013-06-04 14:03:35 -0400

    Make grant_perms command complain about bogus collection names

--- anthropod/collect/management/commands/grant_perms.py
+++ anthropod/collect/management/commands/grant_perms.py
@@ -2,7 +2,7 @@ from optparse import make_option
 
 from django.core.management.base import BaseCommand
 
-from anthropod.core import user_db
+from anthropod.core import db, user_db
 from anthropod.collect.permissions import grant_permissions, revoke_permissions
 
 
@@ -23,24 +23,57 @@ class Command(BaseCommand):
         help='Revoke permissions instead of granting them.'),
     )
 
-    def handle(self, usernames, ocd_ids, permissions, **options):
+    collection_names = db.collection_names()
+    collection_names.remove('system.indexes')
+
+    def handle(self, usernames, permissions, ocd_ids=None, **options):
+
+        permissions = permissions.split(',')
+        self.check_collection_names(permissions)
+        self.check_usernames(usernames)
 
         # Granting or revoking?
         if options['revoke']:
-            self.revoke(usernames, ocd_ids, permissions.split(','))
+            self.revoke(usernames, ocd_ids, permissions)
         else:
-            self.grant(usernames, ocd_ids, permissions.split(','))
+            self.grant(usernames, ocd_ids, permissions)
+
+    def check_usernames(self, usernames):
+        for username in usernames.split(','):
+            if '@' not in username:
+                msg = 'Usernames should be email addresses, not %r.'
+                raise ValueError(msg % username)
+
+    def check_collection_names(self, permissions):
+        # Complain if the collection name is mistyped.
+        for permission in permissions:
+            collection_name, operation = permission.split('.')
+            if collection_name not in self.collection_names:
+                suggestions = map(repr, self.collection_names)
+                msg = ("Can't grant permissions on collection %r; "
+                       "there is no such collection. Choose from %s.")
+                args = (collection_name, ' or '.join(suggestions))
+                raise ValueError(msg % args)
 
     def grant(self, usernames, ocd_ids, permissions):
         for username in usernames.split(','):
+            msg = 'Granting permissions: %r'
+            if ocd_ids is None:
+                self.stdout.write(msg % [username, permissions])
+                grant_permissions(username, ocd_ids, *permissions)
+                return
             for ocd_id in ocd_ids.split(','):
-                msg = 'Granting permissions: %r'
                 self.stdout.write(msg % [username, ocd_id, permissions])
                 grant_permissions(username, ocd_id, *permissions)
 
     def revoke(self, usernames, ocd_ids, permissions):
         for username in usernames.split(','):
+            msg = 'Revoking permissions: %r'
+            if ocd_ids is None:
+                self.stdout.write(msg % [username, ocd_ids, permissions])
+                revoke_permissions(username, ocd_ids, *permissions)
+                return
+
             for ocd_id in ocd_ids.split(','):
-                msg = 'Revoking permissions: %r'
                 self.stdout.write(msg % [username, ocd_id, permissions])
                 revoke_permissions(username, ocd_id, *permissions)
