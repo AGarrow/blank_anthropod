f3dda11493449b8d842495038758244f4e5135b3
Thom Neale <twneale@gmail.com>
2013-06-04 14:35:36 -0400

    make log_change available on RestrictedView class

--- anthropod/collect/views/base.py
+++ anthropod/collect/views/base.py
@@ -3,11 +3,13 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
 from ..permissions import check_permissions
+from .utils import log_change
 
 
 class RestrictedView(View):
 
     check_permissions = staticmethod(check_permissions)
+    log_change = staticmethod(log_change)
 
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
