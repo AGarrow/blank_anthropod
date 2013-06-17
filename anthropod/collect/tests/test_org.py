26a3b7461d33d2633878e2fe952256e752bde876
Thom Neale <twneale@gmail.com>
2013-06-17 16:15:17 -0400

    Move authorized user tests into a mixin

--- anthropod/collect/tests/test_org.py
+++ anthropod/collect/tests/test_org.py
@@ -6,13 +6,13 @@ from django.core.urlresolvers import reverse, resolve
 import anthropod.core
 import anthropod.tests
 
-from anthropod.collect.views.organization import jsonview
+from anthropod.collect.views import organization
 
 from .fixtures import load_test_fixtures
 
 
 class Mixin(object):
-    '''This class just provides names and data common to all the tests
+    '''This class provides names and data common to all the tests
     in this module.
     '''
     fixtures = ['test_auth.json']
@@ -25,6 +25,28 @@ class Mixin(object):
         self.client = Client()
         self.client.login(**self.login_credentials)
         self.db = anthropod.core.db
+
+    params = [
+        (u'contact_type', ['address', u'cell']),
+        (u'contact_note', ['Not a real address', u'Fake phone']),
+        (u'contact_value', ['123 Example Street', u'(123)456-7890']),
+        (u'source_url', [
+            'http://mayor.cityofboise.org/city-council/',
+            u'https://sunfoundation.com'
+            ]),
+        (u'source_note', [
+            '',
+            u"Sunlight's url"
+            ]),
+        (u'geography_id', u'ocd-division/country:us/state:id/place:boise_city'),
+        (u'name', u'City Council')
+        ]
+
+
+class AuthorizedMixin(Mixin):
+    '''This class defines tests that are common to admin users and
+    authorized users that have explicit permissions to edit things.
+    '''
 
     expected = {
         u'_type': u'organization',
@@ -45,27 +67,6 @@ class Mixin(object):
             {u'note': u"Sunlight's url",
              u'url': u'https://sunfoundation.com'}]}
 
-    params = [
-        (u'contact_type', ['address', u'cell']),
-        (u'contact_note', ['Not a real address', u'Fake phone']),
-        (u'contact_value', ['123 Example Street', u'(123)456-7890']),
-        (u'source_url', [
-            'http://mayor.cityofboise.org/city-council/',
-            u'https://sunfoundation.com'
-            ]),
-        (u'source_note', [
-            '',
-            u"Sunlight's url"
-            ]),
-        (u'geography_id', u'ocd-division/country:us/state:id/place:boise_city'),
-        (u'name', u'City Council')
-        ]
-
-
-class AdminUserTest(Mixin, TestCase):
-
-    login_credentials = dict(username='user1', password='password1')
-
     def test_edit(self):
         '''This test verifies that a new person created with
         the form will get saved as the expected popolo data object.
@@ -81,7 +82,7 @@ class AdminUserTest(Mixin, TestCase):
         location = dict(resp.items())['Location']
         location = re.sub('http://testserver', '', location)
         redirect_view = resolve(location).func
-        self.assertEquals(redirect_view, jsonview)
+        self.assertEquals(redirect_view, organization.jsonview)
 
         # Now verify that the saved popolo object
         # matches what we expect.
@@ -92,38 +93,35 @@ class AdminUserTest(Mixin, TestCase):
 
         self.assertEquals(saved, self.expected)
 
+    def test_delete(self):
+        # Create a new organization.
+        url = reverse('organization.delete')
+        params = [(
+            '_id',
+            'ocd-organization/1bfc9aac-b29b-11e2-9e8b-12313d2facc4')]
+        resp = self.client.post(url, dict(params))
 
-class AuthorizedUserTest(Mixin, TestCase):
+        # Make sure we redirected after the save.
+        self.assertEquals(resp.status_code, 302)
+
+        # Verify that the redirect was to the org listing.
+        location = dict(resp.items())['Location']
+        location = re.sub('http://testserver', '', location)
+        redirect_view = resolve(location).func
+        self.assertEquals(redirect_view, organization.listing)
+
+
+class AdminUserTest(AuthorizedMixin, TestCase):
+    '''The tests on this class run as a user with admin priviliges.
+    '''
+    login_credentials = dict(username='user1', password='password1')
+
+
+class AuthorizedUserTest(AuthorizedMixin, TestCase):
     '''These tests are run as a user isn't an admin but has
     permissions to edit all the objects in the database.
     '''
     login_credentials = dict(username='user2', password='password2')
-
-    def test_edit(self):
-        '''This test verifies that a new person created with
-        the form will get saved as the expected popolo data object.
-        '''
-        # Create a new organization.
-        url = reverse('organization.edit')
-        resp = self.client.post(url, dict(self.params))
-
-        # Make sure we redirected after the save.
-        self.assertEquals(resp.status_code, 302)
-
-        # Verify that the redirect was to the org jsonview.
-        location = dict(resp.items())['Location']
-        location = re.sub('http://testserver', '', location)
-        redirect_view = resolve(location).func
-        self.assertEquals(redirect_view, jsonview)
-
-        # Now verify that the saved popolo object
-        # matches what we expect.
-        ocd_id = re.search(r'ocd-organization.+', location).group()
-        ocd_id = ocd_id.rstrip('/')
-        saved = self.db.organizations.find_one(ocd_id)
-        saved.pop('_id')
-
-        self.assertEquals(saved, self.expected)
 
 
 class UnauthorizedUserTest(Mixin, TestCase):
@@ -141,4 +139,15 @@ class UnauthorizedUserTest(Mixin, TestCase):
         resp = self.client.post(url, dict(self.params))
 
         # Make sure the unauthorized user gets a 403 page.
+        self.assertEquals(resp.status_code, 403)
+
+    def test_delete(self):
+        # Create a new organization.
+        url = reverse('organization.delete')
+        params = [(
+            '_id',
+            'ocd-organization/1bfc9aac-b29b-11e2-9e8b-12313d2facc4')]
+        resp = self.client.post(url, dict(params))
+
+        # Make sure we redirected after the save.
         self.assertEquals(resp.status_code, 403)
