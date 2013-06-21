fac05e1fc3d438e4fb0a44cdeab65783d1d883ed
Thom Neale <twneale@gmail.com>
2013-06-21 17:08:59 -0400

    hyperlink ocd_id and hrefs in jsonview

--- anthropod/collect/templatetags/customtags.py
+++ anthropod/collect/templatetags/customtags.py
@@ -1,3 +1,5 @@
+import re
+import json
 from django import template
 
 
@@ -14,3 +16,43 @@ def button(obj, viewname, edit_or_delete, method='GET'):
         'delete': 'icon-remove'
         }[edit_or_delete]
     return locals()
+
+
+class JsonviewFormatter(object):
+
+    def __init__(self, obj):
+        self.data = json.dumps(obj, indent=4)
+
+    def ocd_id_replacer(self, matchobj):
+        tmpl = '<a href="/collect/{app}/{viewname}/{ocd_id}">{ocd_id}</a>'
+        type_ = matchobj.group(1)
+        ocd_id = matchobj.group()
+        app = {'person': 'person',
+                'organization': 'orgs',
+                'division': 'geo'}[type_]
+        viewname = dict(division='detail').get(type_, 'jsonview')
+        return tmpl.format(app=app, ocd_id=ocd_id, viewname=viewname)
+
+    def ocd_ids(self):
+        '''Hyperlink any ocd_id's found in the json.
+        '''
+        rgx = 'ocd-([a-z]+).{,100}'
+        self.data = re.sub(rgx, self.ocd_id_replacer, self.data)
+
+    def link_replacer(self, matchobj):
+        tmpl = '<a href="{0}">{0}</a>'
+        return tmpl.format(matchobj.group(1))
+
+    def links(self):
+        rgx = '(http://.+?)"'
+        self.data = re.sub(rgx, self.link_replacer, self.data)
+
+    def format(self):
+        self.ocd_ids()
+        self.links()
+        return self.data
+
+
+@register.filter
+def jsonview(obj):
+    return JsonviewFormatter(obj).format()
